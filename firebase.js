dae9c68604bd5b5a12a15d8dd4b59c62687e6ba9
gencbirserhat <gencserhat6@gmail.com>
2024-06-11 01:52:31 +0300

    final project

--- firebase.js
+++ firebase.js
@@ -9,12 +9,12 @@ import 'firebase/compat/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCDqwPfg0CBf2P_EABzpAWEchIqDZJhEko",
-  authDomain: "tarif-sepeti-o.firebaseapp.com",
-  projectId: "tarif-sepeti-o",
-  storageBucket: "tarif-sepeti-o.appspot.com",
-  messagingSenderId: "1072785350982",
-  appId: "1:1072785350982:web:3b781299bd40f83c794547"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
